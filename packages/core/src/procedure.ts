07bb2d754c2d0de1dbf1968747777da47fdeb61b
Sourav12061999 <inno@Mac.lan>
2025-12-28 09:29:14 +0530

    feat: describe added

--- packages/core/src/procedure.ts
+++ packages/core/src/procedure.ts
@@ -1,6 +1,6 @@
 import { z, ZodType } from "zod";
 import { applyProjection } from './projection';
-import { getErrorFromUnknown, SELIX_HTTP_STATUS_MAP, SelixError } from "./SelixError";
+import { getErrorFromUnknown, SELIX_HTTP_STATUS_MAP } from "./SelixError";
 
 export type ProcedureType = 'query' | 'mutation';
 
@@ -9,16 +9,19 @@ export type ProcedureResolver<InputType, OutputType> = (opts: { input: InputType
 export class ProcedureDef<InputType = any, OutputType = any> {
     type: ProcedureType;
     input: ZodType<InputType>;
+    describe: string | Record<string, string> | undefined;
     resolver: ProcedureResolver<InputType, OutputType>;
 
     constructor(
         type: ProcedureType,
         input: ZodType<InputType>,
-        resolver: ProcedureResolver<InputType, OutputType>
+        resolver: ProcedureResolver<InputType, OutputType>,
+        describe?: string | Record<string, string> | undefined,
     ) {
         this.type = type;
         this.input = input;
         this.resolver = resolver;
+        this.describe = describe;
     }
 
     async call(opts: { input: any; project?: any }) {
@@ -51,6 +54,7 @@ export class ProcedureDef<InputType = any, OutputType = any> {
 
 export class ProcedureBuilder<InputType = any> {
     private _input?: ZodType<InputType>;
+    private _describe?: string | Record<string, string> | undefined;
 
     input<T extends ZodType<any>>(schema: T): ProcedureBuilder<z.infer<T>> {
         const newBuilder = new ProcedureBuilder<z.infer<T>>();
@@ -58,11 +62,18 @@ export class ProcedureBuilder<InputType = any> {
         return newBuilder;
     }
 
+    describe(desc: string | Record<string, string> | undefined): ProcedureBuilder<InputType> {
+        const newBuilder = new ProcedureBuilder<InputType>();
+        newBuilder._describe = desc;
+        return newBuilder;
+    }
+
     query<TOutput>(resolver: ProcedureResolver<InputType, TOutput>): ProcedureDef<InputType, TOutput> {
         return new ProcedureDef(
             'query',
             this._input as any ?? z.undefined(),
-            resolver
+            resolver,
+            this._describe
         );
     }
 
@@ -70,7 +81,8 @@ export class ProcedureBuilder<InputType = any> {
         return new ProcedureDef(
             'mutation',
             this._input as any ?? z.undefined(),
-            resolver
+            resolver,
+            this._describe
         );
     }
 }
